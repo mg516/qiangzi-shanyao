ae65b56fe89e92627d3186f497c2a4c84624bf0f
richard <richardxv@qq.com>
2019-01-12 16:07:48 +0800

    1.0.2

--- miniprogram/pages/banner/banner.js
+++ miniprogram/pages/banner/banner.js
@@ -7,14 +7,20 @@ Page({
       'cloud://qz-915205.717a-qz-915205/images/4-8.png',
       'cloud://qz-915205.717a-qz-915205/images/5-8.png',
       'cloud://qz-915205.717a-qz-915205/images/9-8.png',
-      'cloud://qz-915205.717a-qz-915205/images/10-8.png',
+      // 'cloud://qz-915205.717a-qz-915205/images/10-8.png',
       'cloud://qz-915205.717a-qz-915205/images/11-8.png',
       'cloud://qz-915205.717a-qz-915205/images/13.jpg',
       'cloud://qz-915205.717a-qz-915205/images/14.jpg',
       'cloud://qz-915205.717a-qz-915205/images/15.png',
+      'cloud://qz-915205.717a-qz-915205/images/16.jpg',
+      'cloud://qz-915205.717a-qz-915205/images/17.jpg',
+      'cloud://qz-915205.717a-qz-915205/images/18.jpg',
+      'cloud://qz-915205.717a-qz-915205/images/19.jpg',
+      'cloud://qz-915205.717a-qz-915205/images/21.jpg',
       // 'cloud://qz-915205.717a-qz-915205/images/6.png',
       // 'cloud://qz-915205.717a-qz-915205/images/7.png'
     ],
+    audio:null,
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
@@ -23,9 +29,9 @@ Page({
   },
   changePlay(){
     if(!this.data.playState){
-      this.audio.play()
+      this.data.audio.play()
     }else{
-      this.audio.stop()
+      this.data.audio.stop()
     }
     this.setData({
       playState: !this.data.playState
@@ -33,12 +39,23 @@ Page({
   },
   onShow(){
     var res = wx.getSystemInfoSync()
+    let audio = null;
     if (res.platform == 'ios') {
-      this.audio = wx.getBackgroundAudioManager()
+      audio = wx.getBackgroundAudioManager()
     } else {
-      this.audio = wx.createInnerAudioContext();
+      audio = wx.createInnerAudioContext();
     }
-    this.audio.src = 'cloud://qz-915205.717a-qz-915205/audio/daojianrumeng.mp3';
-    this.audio.play();
+    audio.title = '起风了';
+    audio.src = 'cloud://qz-915205.717a-qz-915205/audio/qifengle.mp3';
+    // audio.src = 'cloud://qz-915205.717a-qz-915205/audio/daojianrumeng.mp3';
+    audio.play();
+    this.setData({
+      audio: audio
+    })
+  },
+  callphone(){
+    wx.makePhoneCall({
+      phoneNumber: '18080882172'
+    })
   }
 })
